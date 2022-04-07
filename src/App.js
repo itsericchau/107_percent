ba50499be180614fd0572ac21fd3b84966a1d867
cchau <chun.wing.chau@gmail.com>
2022-04-07 10:49:16 +1000

    API calls rendering

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import "./App.css"
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { Routes, Route, Link } from "react-router-dom"
 import Home from "./Home"
 import Standings from "./Standings"
 
-function App() {
-  const [characterName, setCharacterName] = useState("")
 
+function App() {
   return (
     <div className="App">
       <h1>107%</h1>
       <nav className="navigation">
         <Link to="/">Home</Link>
-        <Link to="/standings">Standings</Link>
+        <Link to="/standings">Standings</Link>  
       </nav>
 
       <Routes>
--- src/Standings.js
+++ src/Standings.js
@@ -1,25 +1,41 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import axios from "axios"
 
 function Standings() {
-  const [standings, setStandings] = useState("")
+  const [standings, setStandings] = useState({})
 
   const getStandings = () => {
-    axios
-      .get("http://ergast.com/api/f1/current/driverStandings.json")
-      .then((res) => {
-        setStandings(res.data)
-      })
+    return axios
+      .get("https://ergast.com/api/f1/current/driverStandings.json")
+      .then((res) => res.data)
   }
+
+  useEffect(() => {
+    getStandings().then((data) => setStandings(data))
+  }, [])
 
   return (
     <div className="standings">
-      <h1>This is Standings Component</h1>
-      <button onClick={getStandings}>getStandings</button>
-      <p>
-        Season:
-      </p>
-      <p></p>
+      <h1>Standings</h1>
+      {standings.MRData && (
+        <ul>
+          <li>
+            Season: {standings.MRData.StandingsTable.StandingsLists[0].season}
+          </li>
+          <li>
+            Round: {standings.MRData.StandingsTable.StandingsLists[0].round}
+          </li>
+          {standings.MRData.StandingsTable.StandingsLists[0].DriverStandings.map(
+            (driver, i) => (
+              <div key={i}>
+                <li>{driver.position}</li>
+                <li>{driver.Driver.givenName}</li>
+                <li>{driver.Driver.familyName}</li>
+              </div>
+            )
+          )}
+        </ul>
+      )}
     </div>
   )
 }
